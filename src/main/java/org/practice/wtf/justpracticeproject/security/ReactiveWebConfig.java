af8bc14ec46df12ec611dfcf960f528b928cada5
= <igi2131@gmail.com>
2020-11-24 20:42:26 +0300

    added functional endpoints, stil haven't implemented validation

--- src/main/java/org/practice/wtf/justpracticeproject/security/ReactiveWebConfig.java
+++ src/main/java/org/practice/wtf/justpracticeproject/security/ReactiveWebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
 @EnableWebFluxSecurity
 @Configuration

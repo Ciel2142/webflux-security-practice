53fdfa9ee11e668967fcd97b387ed7a2ff40373e
= <igi2131@gmail.com>
2020-11-24 14:20:34 +0300

    /register throws needed exception

--- src/main/java/org/practice/wtf/justpracticeproject/security/CustomUserDetail.java
+++ src/main/java/org/practice/wtf/justpracticeproject/security/CustomUserDetail.java
@@ -2,38 +2,31 @@ package org.practice.wtf.justpracticeproject.security;
 
 import org.practice.wtf.justpracticeproject.domain.CustomUser;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
-
 
 public class CustomUserDetail implements UserDetails {
 
-    private final String userName;
-    private final String password;
-    private final List<SimpleGrantedAuthority> roles;
+    private final CustomUser user;
 
     public CustomUserDetail(CustomUser user) {
-        this.roles = user.getAuthorities();
-        this.password = user.getPassword();
-        this.userName = user.getUserName();
+        this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles;
+        return this.user.getAuthorities();
     }
 
     @Override
     public String getPassword() {
-        return this.password;
+        return this.user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return this.userName;
+        return this.user.getUserName();
     }
 
     @Override
